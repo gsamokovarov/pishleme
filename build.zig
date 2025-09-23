dab17ba29ffb8ae542624f1d77503bf9b63a2db8
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-09-23 10:10:49 +0300

    Bring bach zig build x86

--- build.zig
+++ build.zig
@@ -15,6 +15,26 @@ pub fn build(b: *std.Build) void {
     exe.linkLibC();
     b.installArtifact(exe);
 
+    // Add x86_64 target for Intel Macs
+    const x86_target = b.resolveTargetQuery(.{
+        .cpu_arch = .x86_64,
+        .os_tag = .macos,
+    });
+
+    const exe_x86 = b.addExecutable(.{
+        .name = "pishleme-x86_64",
+        .root_module = b.addModule("pishleme-x86", .{
+            .root_source_file = b.path("src/main.zig"),
+            .target = x86_target,
+            .optimize = optimize,
+        }),
+    });
+    exe_x86.linkLibC();
+
+    const install_x86 = b.addInstallArtifact(exe_x86, .{});
+    const x86_step = b.step("x86", "Build for x86_64 (Intel Macs)");
+    x86_step.dependOn(&install_x86.step);
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
